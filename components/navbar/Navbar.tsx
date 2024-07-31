769d8d657c633a3f19f160f4a9b63a5867646699
gopal-codes <gopalraut9840285859@gmail.com>
2024-07-31 13:41:37 +0545

    logo changed and whole footer chagned

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import logo from "../../assets/logo.jpg"
+import logo from "../../assets/tslogo-final1.png"
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
