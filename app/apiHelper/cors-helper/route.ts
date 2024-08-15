d0f8922d7a8ea61247c74ee6ddad0f967a8724dd
mkchauhan647 <mkchauhan647@gmail.com>
2024-08-15 16:41:39 +0545

    initials

--- app/apiHelper/cors-helper/route.ts
+++ app/apiHelper/cors-helper/route.ts
@@ -5,14 +5,12 @@ import type { NextRequest } from 'next/server';
 import axios from 'axios';
 
 export async function POST(req: NextRequest,res: NextResponse) {
-    // Determine if the request is for a proxied route
 
     // console.log('req.nextUrl.pathname', req.nextUrl.pathname);
 
     // console.log('req.body', await req.json());
     const id = await req.json();
 
-    // Construct the external URL
     const externalUrl = `https://www.technicalsewa.com/techsewa/publiccontrol/publicsales/getsalesparts`;
 
     try {
@@ -29,13 +27,11 @@ export async function POST(req: NextRequest,res: NextResponse) {
         const data = await response.json();
         // console.log('response', data);
 
-        // Forward the response from the external API to the client
         return NextResponse.json(data, { status: response.status });
     } catch (error: any) {
         console.error('Error fetching data:', error.message);
         return NextResponse.json({ message: error.message }, { status: error.response?.status || 500 });
     }
 
-    // Let other requests pass through
-    // return NextResponse.next();
+    
 }
