912244ee79eb7a985410bb46d0b442f1f7b2c807
Sandeep Joshi <joshisandip111@gmail.com>
2024-09-12 14:51:08 +0545

    detail page image fix

--- components/product-detail/Detail.tsx
+++ components/product-detail/Detail.tsx
@@ -514,7 +514,7 @@ const Detail: React.FC<DetailsProps> = ({ product, id }) => {
                   <img
                     alt="Product Image"
                     className="w-full h-full lg:object-contain sm:object-cover"
-                    src={product?.image_name ? product.image_name : logo.src}
+                    src={mainImage || logo.src}
                     ref={mainImageRef}
                     onMouseMove={handleMouseMove}
                     onMouseEnter={() => setZoomVisible(true)}
@@ -557,7 +557,7 @@ const Detail: React.FC<DetailsProps> = ({ product, id }) => {
                       className={`w-[70px] h-[60px] rounded-lg ${
                         mainImage === thumbnail ? "border border-cyan-400" : ""
                       }`}
-                      src={product?.image_name ? product.image_name : logo.src}
+                      src={thumbnail || logo.src}
                       style={{
                         aspectRatio: "70/50",
                         objectFit: "cover",
