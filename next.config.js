62cd31ed1550684b4c7c9006f39e7710603cd73b
Manoj Kumar Chauhan <44749661+mkchauhan647@users.noreply.github.com>
2025-01-10 22:23:46 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,10 +6,12 @@ module.exports = {
         source: '/api/:path*', // Match all paths that start with /api
         destination: 'https://www.technicalsewa.com/:path*', // Forward them to the external API
       },
-      {
-        source: '/spareparts/:path*', // Match all paths that start with /spareparts
-        destination: 'http://localhost:3000/spareparts/:path*', // Forward them to your local server
-      },
+      // {
+      //   source: '/spareparts/:path*', // Match all paths that start with /spareparts
+      //   destination: 'http://localhost:3000/spareparts/:path*', // Forward them to your local server
+      // },
+      //jjj
+      //testing 
       // {
       //   source: '/:path*', // Catch-all for any other paths
       //   destination: 'https://www.technicalsewa.com/:path*', // Forward them to the external server
